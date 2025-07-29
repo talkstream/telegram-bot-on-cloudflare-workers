3f1fbd4bbb74bdcaba81a29f89a93f78e74ed95b
Arseniy Kamyshev <nafigator@gmail.com>
2025-07-29 22:17:59 +0700

    fix: remove exports of deleted legacy core files
    - Removed exports of bot.js and telegram-adapter.js from core index
    - These files were deleted in previous commit
    - Fixes TypeScript compilation errors in PR validation

--- src/core/index.ts
+++ src/core/index.ts
@@ -5,5 +5,3 @@
 export * from './interfaces/index.js';
 export * from './events/index.js';
 export * from './plugins/index.js';
-export { createBot } from './bot.js';
-export { TelegramAdapter } from './telegram-adapter.js';
